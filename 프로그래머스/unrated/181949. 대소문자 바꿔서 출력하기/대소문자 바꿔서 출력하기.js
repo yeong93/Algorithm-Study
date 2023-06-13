4780e833c937d93a3f7e2b88d22689fe946fa1c4
yeong93 <64456407+yeong93@users.noreply.github.com>
2023-06-13 22:14:17 +0900

    [unrated] Title: 대소문자 바꿔서 출력하기, Time: 59.46 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    
+    let result = "";
+    for(let i=0; i<str.length; i++){
+        if(str[i]  === str[i].toLowerCase()){
+            result += str[i].toUpperCase();
+        }else{
+            result += str[i].toLowerCase();
+        }
+        
+    }
+    console.log(result);
+});
