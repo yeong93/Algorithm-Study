e02e437a3879edfbb0879a710e030dc20015813b
Sim sukyeong <64456407+yeong93@users.noreply.github.com>
2023-07-05 20:09:00 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -1,8 +1,6 @@
 function solution(num_list) {
-    var answer = 0;
     let even = [];
     let odd = [];
     num_list.filter(item => item % 2 === 0 ? even.push(item) : odd.push(item));
-    answer = Number(even.join('')) + Number(odd.join(''));
-    return answer;
+    return Number(even.join('')) + Number(odd.join(''));
 }
