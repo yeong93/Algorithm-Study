43a807909c6d41563176348e9b4c2bab49b1b8ce
Sim sukyeong <64456407+yeong93@users.noreply.github.com>
2023-07-30 12:32:57 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.js
@@ -1,7 +1,1 @@
-function solution(str_list, ex) {
-    var answer = '';
-    str_list.forEach(function(str){
-        if(!str.includes(ex)) answer+= str;
-    });
-    return answer;
-}
+const solution = (str_list, ex) => str_list.filter(v => !v.includes(ex)).join('');
