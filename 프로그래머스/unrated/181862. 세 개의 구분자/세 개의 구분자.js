8ea9d655af80b9fb7e1af1a603516b7653869fa7
Sim sukyeong <64456407+yeong93@users.noreply.github.com>
2023-07-31 11:09:07 +0900

    [unrated] Title: 세 개의 구분자, Time: 17.36 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.js
@@ -1,6 +1,5 @@
 function solution(myStr) {
     var answer = [];
-    answer = myStr.split(/[a|b|c]/g).filter(v => v !== '');
-    if(answer.length === 0) answer.push('EMPTY');
-    return answer;
+    answer = myStr.split(/[a|b|c]/g).filter(str => str);
+    return answer.length ? answer : ["EMPTY"];
 }
