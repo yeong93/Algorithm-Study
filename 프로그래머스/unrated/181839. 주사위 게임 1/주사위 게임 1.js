0e0e9384da054ed0cad14e131669cf3fe4cd4a9c
Sukyeong Sim <4ever-young@naver.com>
2023-08-01 12:17:20 +0900

    [unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    var answer = 0;
+    if(a%2!==0 && b%2!==0){
+        answer = Math.pow(a,2) + Math.pow(b,2);
+    }else if(a%2===0 && b%2===0){
+        answer = Math.abs(a-b)
+    }else{
+        answer = 2 * (a+b)
+    }
+    return answer;
+}
