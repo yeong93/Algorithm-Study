707253d6281a01c9b8109cd5f393ccbcf8302692
Sukyeong Sim <4ever-young@naver.com>
2023-08-04 15:19:10 +0900

    [level 0] Title: 합성수 찾기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.js
@@ -1,7 +1,6 @@
 function solution(n) {
-    var answer = 0;
+    let answer = 0;
     for(let i=1; i<=n; i++){
-        let count =0;
         for(let j=2; j<i; j++){
             if(i%j===0){
                 answer++;
