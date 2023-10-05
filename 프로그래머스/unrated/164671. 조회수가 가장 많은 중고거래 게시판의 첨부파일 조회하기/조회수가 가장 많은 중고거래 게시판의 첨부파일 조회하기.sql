42920bc4fc691b36de75deb5e5d631e46c72727e
Sukyeong Sim <4ever-young@naver.com>
2023-10-05 21:23:20 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -0,0 +1,10 @@
+SELECT '/home/grep/src/' || A.BOARD_ID || '/' || FILE_ID || FILE_NAME || FILE_EXT AS FILE_PATH
+  FROM ( SELECT BOARD_ID
+           FROM (SELECT BOARD_ID 
+                   FROM USED_GOODS_BOARD
+                  ORDER BY VIEWS DESC)
+          WHERE ROWNUM <= 1
+      ) A
+     , USED_GOODS_FILE B
+ WHERE A.BOARD_ID = B.BOARD_ID
+ ORDER BY FILE_ID DESC
