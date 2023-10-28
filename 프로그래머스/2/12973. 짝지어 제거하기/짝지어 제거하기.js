10e718695848f5c4409e29d29164b76016c62a42
Sukyeong Sim <4ever-young@naver.com>
2023-10-28 17:35:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 24.18 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,6 +1,7 @@
 function solution(s)
 {
-    const stack = []
+    const stack = [];
+    // forEach 적용시 효율성 떨어지는 case
     for(let i=0;i<s.length;i++){
         if(stack[stack.length - 1] === s[i]){
             stack.pop()
