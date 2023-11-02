9c828f28f088fd1fd822b2ac64978c44a62f3af3
Sukyeong Sim <4ever-young@naver.com>
2023-11-02 14:42:41 +0900

    [level 1] Title: 과일 장수, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,9 @@
+function solution(k, m, score) {
+    var answer = 0;
+    score.sort((a,b) => (b - a));
+    for(let i=m; i<=score.length; i=i+m){
+        answer += score[i-1]*m;
+    }
+    return answer;
+
+}
