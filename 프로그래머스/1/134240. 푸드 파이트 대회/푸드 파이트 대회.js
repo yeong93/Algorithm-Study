8363fb3aea6d9ce5ccdb6a80a55988913dc571d6
Sukyeong Sim <4ever-young@naver.com>
2023-11-22 14:41:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.js
@@ -0,0 +1,9 @@
+function solution(food) {
+    var answer = '';
+    for(let i=1; i<food.length; i++){
+        for(let j=0; j<Math.floor(food[i]/2); j++){
+            answer +=i;
+        }
+    }
+    return answer + '0' +answer.split('').reverse().join('');
+}
